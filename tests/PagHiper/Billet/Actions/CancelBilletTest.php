611edfe68ae99c4ddabefd0294a0c3eab1431620
AJ Meireles <alvaro.meireles@live.com>
2023-07-28 20:54:48 -0300

    wip

--- tests/PagHiper/Billet/Actions/CancelBilletTest.php
+++ tests/PagHiper/Billet/Actions/CancelBilletTest.php
@@ -7,68 +7,65 @@ use Illuminate\Http\Client\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
 
-describe('cancel billet', function () {
+it('should be able to cancel billet casting to array', function () {
+    $transaction = 'BPV661O7AVLORCN5';
 
-    it('should be able to cancel billet casting to array', function () {
-        $transaction = 'BPV661O7AVLORCN5';
+    $result = [
+        'result'           => 'success',
+        'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
+        'http_code'        => '200',
+    ];
 
-        $result = [
-            'result'           => 'success',
-            'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
-            'http_code'        => '200',
-        ];
+    Http::fake([
+        Request::url(CancelBillet::END_POINT) => Http::response([
+            'cancellation_request' => $result,
+        ]),
+    ]);
 
-        Http::fake([
-            Request::url(CancelBillet::END_POINT) => Http::response([
-                'cancellation_request' => $result,
-            ]),
-        ]);
+    $status = (new PagHiper())->billet()->cancel($transaction);
 
-        $status = (new PagHiper())->billet()->cancel($transaction);
+    expect($status)
+        ->toBeArray()
+        ->and($status)
+        ->toBe($result);
+});
 
-        expect($status)
-            ->toBeArray()
-            ->and($status)
-            ->toBe($result);
-    });
+it('should be able to cancel billet casting to collection', function (string $cast) {
+    $transaction = 'BPV661O7AVLORCN5';
 
-    it('should be able to cancel billet casting to collection', function (string $cast) {
-        $transaction = 'BPV661O7AVLORCN5';
+    $result = [
+        'result'           => 'success',
+        'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
+        'http_code'        => '200',
+    ];
 
-        $result = [
-            'result'           => 'success',
-            'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
-            'http_code'        => '200',
-        ];
+    Http::fake([
+        Request::url(CancelBillet::END_POINT) => Http::response([
+            'cancellation_request' => $result,
+        ]),
+    ]);
 
-        Http::fake([
-            Request::url(CancelBillet::END_POINT) => Http::response([
-                'cancellation_request' => $result,
-            ]),
-        ]);
+    $status = (new PagHiper())->billet($cast)->cancel($transaction);
 
-        $status = (new PagHiper())->billet($cast)->cancel($transaction);
+    expect($status)->toBeInstanceOf(Collection::class);
+})->with(['collection', 'collect']);
 
-        expect($status)->toBeInstanceOf(Collection::class);
-    })->with(['collection', 'collect']);
+it('should be able to cancel billet casting to original response', function () {
+    $transaction = 'BPV661O7AVLORCN5';
 
-    it('should be able to cancel billet casting to original response', function () {
-        $transaction = 'BPV661O7AVLORCN5';
+    $result = [
+        'result'           => 'success',
+        'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
+        'http_code'        => '200',
+    ];
 
-        $result = [
-            'result'           => 'success',
-            'response_message' => "O Boleto $transaction foi cancelado com Sucesso",
-            'http_code'        => '200',
-        ];
+    Http::fake([
+        Request::url(CancelBillet::END_POINT) => Http::response([
+            'cancellation_request' => $result,
+        ]),
+    ]);
 
-        Http::fake([
-            Request::url(CancelBillet::END_POINT) => Http::response([
-                'cancellation_request' => $result,
-            ]),
-        ]);
+    $status = (new PagHiper())->billet(cast: 'response')->cancel($transaction);
 
-        $status = (new PagHiper())->billet(cast: 'response')->cancel($transaction);
-
-        expect($status)->toBeInstanceOf(Response::class);
-    });
+    expect($status)->toBeInstanceOf(Response::class);
 });
--- tests/Structure/StructureTest.php
+++ tests/Structure/StructureTest.php
@@ -5,20 +5,18 @@ use DevAjMeireles\PagHiper\Core\Request\Request;
 use DevAjMeireles\PagHiper\Core\Traits\InteractWithCasts;
 use DevAjMeireles\PagHiper\PagHiper;
 
-describe('structure', function () {
-    test('will not debugging functions')
-        ->expect(['dd', 'dump', 'ray'])
-        ->each->not->toBeUsed();
+test('will not debugging functions')
+    ->expect(['dd', 'dump', 'ray'])
+    ->each->not->toBeUsed();
 
-    test('paghiper class should not extends nothing')
-        ->expect(PagHiper::class)
-        ->toExtendNothing();
+test('paghiper class should not extends nothing')
+    ->expect(PagHiper::class)
+    ->toExtendNothing();
 
-    test('request class should be final')
-        ->expect(Request::class)
-        ->toBeFinal();
+test('request class should be final')
+    ->expect(Request::class)
+    ->toBeFinal();
 
-    test('InteractWithCasts trait should be used only in Billet')
-        ->expect(InteractWithCasts::class)
-        ->toOnlyBeUsedIn(Billet::class);
-});
+test('InteractWithCasts trait should be used only in Billet')
+    ->expect(InteractWithCasts::class)
+    ->toOnlyBeUsedIn(Billet::class);
