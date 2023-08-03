c86f9bdd0f90ac565ed48fbc63259d232bd9e13a
AJ Meireles <alvaro.meireles@live.com>
2023-08-02 22:14:52 -0300

    wip

--- src/Facades/PagHiper.php
+++ src/Facades/PagHiper.php
@@ -4,13 +4,16 @@ namespace DevAjMeireles\PagHiper\Facades;
 
 use DevAjMeireles\PagHiper\Billet;
 use DevAjMeireles\PagHiper\Enums\Cast;
+use DevAjMeireles\PagHiper\Pix;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static Billet billet(Cast $cast = Cast::Array)
+ * @method static Pix pix(Cast $cast = Cast::Array)
  * @method static void resolveApiUsing(callable $callback)
  * @method static void resolveTokenUsing(callable $callback)
  * @method static void resolveBilletNotificationlUrlUsing(callable $callback)
+ * @method static void resolvePixNotificationlUrlUsing(callable $callback)
  *
  * @see \DevAjMeireles\PagHiper\PagHiper
  */
